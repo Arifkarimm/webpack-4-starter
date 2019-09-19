7d77bd283f7f8223f5c980c922f7b8312242ccc3
Fazlul <fkarif.cse@gmail.com>
2019-09-19 10:13:39 +0200

    babel plugins install and configure

--- dist/bundle.js
+++ dist/bundle.js
@@ -86,27 +86,14 @@
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/scripts/demo.js":
-/*!*****************************!*\
-  !*** ./src/scripts/demo.js ***!
-  \*****************************/
-/*! exports provided: cube, foo */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"cube\", function() { return cube; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"foo\", function() { return foo; });\nfunction cube(x) {\n  return x * x * x;\n}\n\nvar foo = Math.PI + Math.SQRT2;\n\n\n//# sourceURL=webpack:///./src/scripts/demo.js?");
-
-/***/ }),
-
 /***/ "./src/scripts/index.js":
 /*!******************************!*\
   !*** ./src/scripts/index.js ***!
   \******************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _demo_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./demo.js */ \"./src/scripts/demo.js\");\n\nvar getOutput = console.log('Hello this is me');\nconsole.log(Object(_demo_js__WEBPACK_IMPORTED_MODULE_0__[\"cube\"])(3));\nconsole.log(_demo_js__WEBPACK_IMPORTED_MODULE_0__[\"foo\"]);\nconsole.log(getOutput);\n\n//# sourceURL=webpack:///./src/scripts/index.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/src/scripts/index.js: Support for the experimental syntax 'classProperties' isn't currently enabled (11:4):\\n\\n\\u001b[0m \\u001b[90m  9 | \\u001b[39m\\u001b[36mclass\\u001b[39m \\u001b[33mStudent\\u001b[39m{\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 | \\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 11 | \\u001b[39m\\ta \\u001b[33m=\\u001b[39m \\u001b[35m20\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    | \\u001b[39m\\t  \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 12 | \\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 13 | \\u001b[39m\\ttoFunc(){\\u001b[0m\\n\\u001b[0m \\u001b[90m 14 | \\u001b[39m\\u001b[0m\\n\\nAdd @babel/plugin-proposal-class-properties (https://git.io/vb4SL) to the 'plugins' section of your Babel config to enable transformation.\\n    at Parser.raise (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:6387:17)\\n    at Parser.expectPlugin (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:7709:18)\\n    at Parser.parseClassProperty (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10901:12)\\n    at Parser.pushClassProperty (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10866:30)\\n    at Parser.parseClassMemberWithIsStatic (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10805:14)\\n    at Parser.parseClassMember (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10739:10)\\n    at withTopicForbiddingContext (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10694:14)\\n    at Parser.withTopicForbiddingContext (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:9773:14)\\n    at Parser.parseClassBody (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10671:10)\\n    at Parser.parseClass (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10645:22)\\n    at Parser.parseStatementContent (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:9942:21)\\n    at Parser.parseStatement (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:9900:17)\\n    at Parser.parseBlockOrModuleBlockBody (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10476:25)\\n    at Parser.parseBlockBody (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:10463:10)\\n    at Parser.parseTopLevel (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:9829:10)\\n    at Parser.parse (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:11341:17)\\n    at parse (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/parser/lib/index.js:11377:38)\\n    at parser (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/core/lib/transformation/normalize-file.js:166:34)\\n    at normalizeFile (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/core/lib/transformation/normalize-file.js:100:11)\\n    at runSync (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/core/lib/transformation/index.js:44:43)\\n    at runAsync (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/core/lib/transformation/index.js:35:14)\\n    at process.nextTick (/Users/fazlulkarim/Development/Github-Projects/webpack-4-starter/node_modules/@babel/core/lib/transform.js:34:34)\\n    at process._tickCallback (internal/process/next_tick.js:61:11)\");\n\n//# sourceURL=webpack:///./src/scripts/index.js?");
 
 /***/ })
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,17 @@ const getOutput = console.log('Hello this is me');
 console.log(cube(3)); 
 console.log(foo);
 console.log(getOutput);
+
+class Student{
+
+	a = 20
+
+	toFunc(){
+
+		console.log("test")
+	} 
+}
+
+const a = new Student();
+
+a.toFunc();
